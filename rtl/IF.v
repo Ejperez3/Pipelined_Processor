6d316e4145e0425fd1129502a89a740ff2f50e8c
eperez <ejperez3@wisc.edu>
2025-11-06 12:00:46 -0600

    syntax errors

--- rtl/IF.v
+++ rtl/IF.v
@@ -6,20 +6,19 @@ module IF(
   input wire i_rst,           //used to reset PC to starting value
   input wire [31:0] i_NextPC,      //next adress for PC either PC+4 or branch/jump PC
 
-    output reg [31:0] o_PC,          //current PC will feed into instruction memory and other locations (schematic) 
-    output wire [31:0] o_inc_pc  //Output current PC + 4 
+  output reg [31:0] o_PC,          //current PC will feed into instruction memory and other locations (schematic) 
+  output wire [31:0] o_inc_pc  //Output current PC + 4 
 );
 
-  always @(posedge i_clk) begin
+always @(posedge i_clk) begin
     if (i_rst) begin
-      o_PC <= 32'h00000000;  //on reset gets assigned to adress 0
+        o_PC <= 32'h00000000;  // reset PC
+    end else if (IF_EN) begin
+        o_PC <= i_NextPC;      // update PC when IF_EN is 1
     end else begin
-      //if NOP, maintain current state
-      if(IF_EN)
-        o_PC<=i_NextPC;   
-      else
-        o_PC <= o_PC; 
- end
+        o_PC <= o_PC;           // hold PC (synchronous stall)
+    end
+end
 
   assign o_inc_pc = o_PC + 32'd4;
 
