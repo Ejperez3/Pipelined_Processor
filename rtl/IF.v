bfe46d100323a8f72eebaffccdd4ac58701b24f2
SUBRAMANYA VEDAANT NOOKALA <nookala2@wisc.edu>
2025-11-06 10:13:36 -0600

    added pipeline to PC. however, now results in a few instructions being missed during stalls (memtry fetches/continues to fetch instructions?). added image for reference.

--- rtl/IF.v
+++ rtl/IF.v
@@ -1,27 +1,30 @@
 `default_nettype none
 
-module IF(
-  input wire NOP,
-  input wire i_clk,           //clk to control PC update
-  input wire i_rst,           //used to reset PC to starting value
-  input wire [31:0] i_NextPC,      //next adress for PC either PC+4 or branch/jump PC
+module IF (
+    input wire        NOP,
+    input wire        i_clk,    //clk to control PC update
+    input wire        i_rst,    //used to reset PC to starting value
+    input wire [31:0] i_NextPC, //next adress for PC either PC+4 or branch/jump PC
 
-  output reg [31:0] o_PC,          //current PC will feed into instruction memory and other locations (schematic) 
-  output wire[31:0] o_inc_pc //Output current PC + 4 
+    output reg [31:0] o_PC,          //current PC will feed into instruction memory and other locations (schematic) 
+    output wire [31:0] o_inc_pc  //Output current PC + 4 
 );
 
-always @(posedge i_clk) begin
-    if (i_rst)
-      o_PC <= 32'h00000000;   //on reset gets assigned to adress 0
-    else
+  always @(posedge i_clk) begin
+    if (i_rst) begin
+      o_PC <= 32'h00000000;  //on reset gets assigned to adress 0
+    end else begin
       //if NOP, maintain current state
-      if(~NOP)
-        o_PC <= o_PC;      
-      else
-        o_PC<=i_NextPC;
- end
+      if (~NOP) begin
+        o_PC <= o_PC;
+      end else begin
+        o_PC <= i_NextPC;
+      end
+    end
+  end
 
-assign o_inc_pc = o_PC + 32'd4;
+  assign o_inc_pc = o_PC + 32'd4;
 
 endmodule
 `default_nettype wire
+
